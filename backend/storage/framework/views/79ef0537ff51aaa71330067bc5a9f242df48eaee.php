a87bf4ce2fb0cf8f11c68a8d59aeab749bee30d8
Nimish J <njain@gammastack.com>
2020-06-27 18:28:07 +0530

    initial commit

---
+++ backend/storage/framework/views/79ef0537ff51aaa71330067bc5a9f242df48eaee.php
@@ -0,0 +1,47 @@
+<?php $__env->startSection('content'); ?>
+<tr>
+  <td style="padding:0 15px 0 30px; ">
+    <h3 style="color:#000; font-size:30px; font-family:Calibri; margin:0px; padding:0; font-weight:bold; line-height:50px; ">Hello <?php echo $username; ?>,</h3>
+  </td>
+</tr>
+<tr>
+  <td style="padding:0 30px 10px 30px; font-family:Arial, Helvetica, sans-serif; font-size:14px;">
+    <p style="margin:0px; padding:0px; color:#fff; display:block; font-size:22px;">
+      You've requested instructions to reset your
+      current password. Simply click on the button
+      below and you will be redirect to our
+      application to create your new password.
+    </p>
+  </td>
+</tr>
+<tr>
+  <td style="padding:0 30px 10px 30px; font-weight: bold;font-family:Arial, Helvetica, sans-serif; font-size:14px;">
+    <a href="<?php echo $link; ?>" style="color:#000; text-shadow: 0 0px 1px rgba(0,0,0,0.8);text-decoration:none; cursor:pointer;font-weight: bold;">
+    Create new password 
+    </a>
+  </td>
+</tr>
+<tr>
+  <td style="padding:0 30px 10px 30px; font-weight: bold;font-family:Arial, Helvetica, sans-serif; font-size:14px;">
+    <p style="margin:0px; padding:0px; color:#333333; display:block; font-size:14px;">
+
+    </p>
+    <p style="margin:0px; padding:0px; color:#fff; display:block; font-size:14px;font-weight: bold;">
+      If you did not request to change your current
+      password, please ignore this email and
+      contact us immediately.
+    </p>
+  </td>
+</tr>
+<tr>
+  <td style="padding:0 30px 10px 30px; font-weight: bold;font-family:Arial, Helvetica, sans-serif; font-size:14px;">
+    <p style="margin:0px; padding:0px; color:#333333; display:block; font-size:14px;">
+
+    </p>
+    <p style="margin:0px; padding:0px; color:#fff; display:block; font-size:14px;font-weight: bold;">
+      kindly,
+    </p>
+  </td>
+</tr>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('emails.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/zendraft/front-apis/backend/resources/views/emails/forgot_pass_mail.blade.php ENDPATH**/ ?>
---
+++ backend/storage/framework/views/f72949defb87ef95b82de06d4c79550db06c0c21.php
@@ -0,0 +1,34 @@
+<?php $__env->startSection('content'); ?>
+<tr>
+  <td style="padding:0 15px 0 30px; ">
+    <p style="color:#000; font-size:30px; font-family:Calibri; margin:0px; padding:0; font-weight:bold; line-height:50px; ">Hi <?php echo $username; ?>,</p>
+  </td>
+</tr>
+<tr>
+  <td style="padding:0 30px 10px 30px; font-family:Arial, Helvetica, sans-serif; font-size:14px;">
+    <h3 style="margin:0px; padding:0px; color:#fff; display:block; font-size:22px;">
+      Thanks for signing up at <?php echo env('PROJECT_NAME_MAIL_FORMAT') ; ?>.
+    </h3>
+  </td>
+</tr>
+<tr>
+  <td style="padding:0 30px 10px 30px; font-weight: bold;font-family:Arial, Helvetica, sans-serif; font-size:14px;">
+    Please<a href="<?php echo $link; ?>" style="color:#000; text-shadow: 0 0px 1px rgba(0,0,0,0.8);text-decoration:none; cursor:pointer;font-weight: bold;"> click here</a> to verify your email
+  </td>
+</tr>
+<tr>
+  <td style="padding:0 30px 10px 30px; font-weight: bold;font-family:Arial, Helvetica, sans-serif; font-size:14px;">
+    <p style="margin:0px; padding:0px; color:#333333; display:block; font-size:14px;">
+
+    </p>
+    <p style="margin:0px; padding:0px; color:#fff; display:block; font-size:14px;font-weight: bold;">
+      We are ready for you to join our Adventure.
+    </p>
+  </td>
+</tr>
+<?php $__env->stopSection(); ?>
+
+
+
+
+<?php echo $__env->make('emails.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/zendraft/front-apis/backend/resources/views/emails/signup_emailer.blade.php ENDPATH**/ ?>
