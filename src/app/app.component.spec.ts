317c014dc662e2cb62e928409d75b1389b0ffe6c
Nimish Jain <jainnimish7@gmail.com>
2022-03-03 19:02:53 +0530

    Updated packages and dependencies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front-api'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('front-api');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('front-api app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Boilerplate app is running!');
   });
 });
